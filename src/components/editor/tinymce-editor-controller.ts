4e2be4b0a03ac27787bb988f23365071fccedfe8
Vishesh.Shrivastava <vishesh.shrivastava@rapidops.co>
2024-08-26 12:27:15 +0530

    xss attack security and sanitized html

--- src/components/editor/tinymce-editor-controller.ts
+++ src/components/editor/tinymce-editor-controller.ts
@@ -1,4 +1,5 @@
 //@ts-nocheck
+import DOMPurify from "dompurify";
 import { useEffect, useRef, useState } from "react";
 
 const useTinymceEditorController = () => {
@@ -20,6 +21,12 @@ const useTinymceEditorController = () => {
           <html>
             <head>
               <base href="${window.location.origin}">
+              <!-- SAFE_LINKS 
+              <meta http-equiv="Content-Security-Policy" content="script-src 'unsafe-inline' ${
+                /* SAFE_LINKS */ ""
+              }; object-src 'none';  base-uri 'self';">
+              -->
+              <meta http-equiv="Content-Security-Policy" object-src 'none';  base-uri 'self';">
             </head>
             <body>${content}</body>
           </html>
@@ -54,9 +61,55 @@ const useTinymceEditorController = () => {
     return scripts.length > 0;
   };
 
+  const sanitizeEditorContent = (content) => {
+    // Define dangerous patterns related to storage access, infinite loops, and buggy code
+    const dangerousPatterns = [
+      // Infinite loops
+      /while\s*\(\s*true\s*\)\s*\{/,
+      /for\s*\(\s*;;\s*\)\s*\{/,
+      /setInterval\s*\(\s*function\s*\(\)\s*{[^}]*}\s*,\s*0\s*\)/,
+
+      // Access to storage and cookies
+      /localStorage/,
+      /sessionStorage/,
+      /document\.cookie/,
+
+      // Potentially dangerous functions
+      /window\.alert\s*\(/,
+      /alert\s*\(/,
+      /window\.open\s*\(/,
+      /window\.close\s*\(/,
+      /window\.print\s*\(/,
+      /eval\s*\(/,
+      /Function\s*\(/,
+      /document\.write\s*\(/,
+      /document\.open\s*\(/,
+      /document\.close\s*\(/,
+
+      // XSS patterns
+      /on\w+="/,
+      /on\w+='\s*'/,
+      /javascript:\s*[^'"]+/g,
+
+      // Modifying prototypes and constructors
+      /Object\.prototype\./,
+      /Array\.prototype\./,
+      /Function\.constructor\./,
+    ];
+
+    // Remove any code matching these patterns
+    dangerousPatterns.forEach((pattern) => {
+      content = content.replace(pattern, "/sanitized/");
+    });
+
+    return DOMPurify.sanitize(content, {
+      ADD_TAGS: ["iframe", "script"],
+    });
+  };
+
   const scriptsExecutor = async (editor) => {
     try {
-      const editorContent = editor.getContent();
+      const editorContent = sanitizeEditorContent(editor.getContent());
       //if no scripts then return
       if (!checkScripts(editorContent)) return;
 
